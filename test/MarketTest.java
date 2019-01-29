bdba08cb4dbbc388cc941c97507a21909ac449ea
Hugo BLASCO <hugoblasco@MacBook-Pro-de-Hugo.local>
2019-01-29 23:42:29 +0100

    ajout de la classe de test

---
+++ test/MarketTest.java
@@ -0,0 +1,14 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class MarketTest {
+
+	@Test
+	void test() {
+		Assertions.assertThat(true).isTrue();
+		fail("Not yet implemented");
+	}
+
+}

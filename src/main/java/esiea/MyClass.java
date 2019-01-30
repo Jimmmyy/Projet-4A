5bc1da25a9dffe500595e933caf9bf071eb8124f
Jimmmyy <37215512+Jimmmyy@users.noreply.github.com>
2019-01-30 13:43:36 +0100

    ajout du fichier main

---
+++ src/main/java/esiea/MyClass.java
@@ -0,0 +1,13 @@
+package esiea;
+
+/**
+ * Hello world!
+ *
+ */
+public class MyClass 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

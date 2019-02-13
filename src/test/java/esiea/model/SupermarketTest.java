d09fcbc650187a43fd7102bf3ce03604ddeb201a
Hugo BLASCO <hugoblasco@MacBook-Pro-de-Hugo.local>
2019-02-13 14:52:26 +0100

    mise en place du test du prix des pommes sachant quil y a une reduction pour les brosses a dents

--- src/test/java/esiea/model/SupermarketTest.java
+++ src/test/java/esiea/model/SupermarketTest.java
@@ -25,10 +25,17 @@ public class SupermarketTest {
 
         Receipt receipt = teller.checksOutArticlesFrom(cart);
 
-        // Todo: complete this test
-
-        List<Product> products = Arrays.asList(new Product("toto", ProductUnit.Each));
-        assertThat(true).isTrue();
-        assertThat(products).extracting(Product::getName).as("Product names").containsExactly("apple", "orange");
+        // TODO: complete this test
+        
+        //test : combien coute 2.5 kg de pommes à 1.99€ compte tenu du fait qu'une
+        // réduction est en court sur les brosses à dents.
+        
+        assertThat(receipt.getTotalPrice()).isEqualTo(2.5*1.99);
+        // WOW CA MARCHE
+        
+        
+        //List<Product> products = Arrays.asList(new Product("toto", ProductUnit.Each));
+        //assertThat(true).isTrue();
+        //assertThat(products).extracting(Product::getName).as("Product names").containsExactly("apple", "orange");
     }
 }

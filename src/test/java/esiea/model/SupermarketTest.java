4144a691c41db482160ff31757aa5d3b8cfb7ba4
Tourneroche <43216480+Tourneroche@users.noreply.github.com>
2019-02-17 21:33:13 +0100

    product test equals test

--- src/test/java/esiea/model/SupermarketTest.java
+++ src/test/java/esiea/model/SupermarketTest.java
@@ -244,4 +244,29 @@ public class SupermarketTest {
 
         Assertions.assertThat(testDiscount.getProduct()).isEqualTo(apples);
     }
+    
+    
+    @Test
+    public void testEqualsProduct () {
+
+        Product apples = new Product("apples", ProductUnit.Kilo);
+
+        Discount discount = new Discount(apples, "Apples Discount", 0.199);
+
+        Product banane = new Product("banane", ProductUnit.Kilo);
+        Product toothbrush = new Product("toothbrush", ProductUnit.Each);
+        Product apples1 = new Product("apples", ProductUnit.Kilo);
+
+        Assertions.assertThat(apples.equals(apples)).isEqualTo(true);
+        Assertions.assertThat(apples.equals(null)).isEqualTo(false);
+
+        Assertions.assertThat(apples.equals(discount)).isEqualTo(false);
+        Assertions.assertThat(apples.equals(banane)).isEqualTo(false);
+        Assertions.assertThat(apples.equals(toothbrush)).isEqualTo(false);
+        Assertions.assertThat(apples.equals(apples1)).isEqualTo(true);
+
+
+    }
+    
+    
 }

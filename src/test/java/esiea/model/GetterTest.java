60c47d6b41d1c3add66e4b4d7a21b510761367c2
Tourneroche <43216480+Tourneroche@users.noreply.github.com>
2019-02-24 14:47:46 +0100

    Restructuration des Tests, création getterTest

---
+++ src/test/java/esiea/model/GetterTest.java
@@ -0,0 +1,83 @@
+package esiea.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class GetterTest {
+	
+	 @Test
+	    public void testGetterItemsReceipt(){
+	    	Receipt ticket= new Receipt();
+	    	List<ReceiptItem> items= new ArrayList<>();
+	    	List<ReceiptItem> itemsdelobjet=ticket.getItems();
+	    	
+	    	assertEquals(items,itemsdelobjet);
+	    }
+	    
+	    @Test
+	    public void testGetterDiscountsReceipt(){
+	    	
+	    	Receipt ticket= new Receipt();
+	    	List<Discount> discounts = new ArrayList<>();
+	    	List<Discount> discountsdelobjet = ticket.getDiscounts();
+	    	
+	    	assertEquals(discounts,discountsdelobjet);
+	    }
+	    
+	    @Test
+	    public void testGetterPrice(){
+	    	
+	    	Product product=new Product("pomme",ProductUnit.Kilo);
+	    	ReceiptItem ticket= new ReceiptItem(product,2.0,10.0,20.0);
+	    	double prix=ticket.getPrice();
+	    	double prix2=10.0;
+	    	assertEquals(prix,prix2);
+	    }
+	    
+	    @Test
+	    
+	    public void testGetterProduct(){
+	    	Product product=new Product("pomme",ProductUnit.Kilo);
+	    	ReceiptItem ticket= new ReceiptItem(product,2.0,10.0,20.0);
+	    	Product verifProduct = ticket.getProduct();
+	    	assertEquals(product,verifProduct);
+	    }
+	    
+	    
+	    @Test
+	    
+	    public void testGetterQuantity(){
+	    	Product product=new Product("pomme",ProductUnit.Kilo);
+	    	ReceiptItem ticket1= new ReceiptItem(product,2.0,10.0,20.0);
+	    	double verifquantityproduct = ticket1.getQuantity();
+	    	assertEquals(2.0, verifquantityproduct);
+	    	
+	    }
+	    
+	    
+	    @Test
+	    public void getDescriptiontest(){
+	        Product apples = new Product("apples", ProductUnit.Kilo);
+	        Discount testDiscount = new Discount(apples, "Apples Discount", 1.99);
+
+	        Assertions.assertThat(testDiscount.getDescription()).isEqualTo("Apples Discount");
+	    }
+	 
+	    
+	    @Test
+	    public void getProducttest(){
+	        Product apples = new Product("apples",ProductUnit.Kilo);
+	        Discount testDiscount = new Discount(apples, "Apples Discount", 1.99);
+
+	        Assertions.assertThat(testDiscount.getProduct()).isEqualTo(apples);
+	    }
+	    
+	
+	
+
+}
--- src/test/java/esiea/model/SupermarketTest.java
+++ src/test/java/esiea/model/SupermarketTest.java
@@ -161,55 +161,7 @@ public class SupermarketTest {
     	assertEquals(Unite,ProductUnit.Kilo);
     }
     
-    @Test
-    public void testGetterItemsReceipt(){
-    	Receipt ticket= new Receipt();
-    	List<ReceiptItem> items= new ArrayList<>();
-    	List<ReceiptItem> itemsdelobjet=ticket.getItems();
-    	
-    	assertEquals(items,itemsdelobjet);
-    }
-    
-    @Test
-    public void testGetterDiscountsReceipt(){
-    	
-    	Receipt ticket= new Receipt();
-    	List<Discount> discounts = new ArrayList<>();
-    	List<Discount> discountsdelobjet = ticket.getDiscounts();
-    	
-    	assertEquals(discounts,discountsdelobjet);
-    }
-    
-    @Test
-    public void testGetterPrice(){
-    	
-    	Product product=new Product("pomme",ProductUnit.Kilo);
-    	ReceiptItem ticket= new ReceiptItem(product,2.0,10.0,20.0);
-    	double prix=ticket.getPrice();
-    	double prix2=10.0;
-    	assertEquals(prix,prix2);
-    }
-    
-    @Test
-    
-    public void testGetterProduct(){
-    	Product product=new Product("pomme",ProductUnit.Kilo);
-    	ReceiptItem ticket= new ReceiptItem(product,2.0,10.0,20.0);
-    	Product verifProduct = ticket.getProduct();
-    	assertEquals(product,verifProduct);
-    }
-    
-    
-    @Test
-    
-    public void testGetterQuantity(){
-    	Product product=new Product("pomme",ProductUnit.Kilo);
-    	ReceiptItem ticket1= new ReceiptItem(product,2.0,10.0,20.0);
-    	double verifquantityproduct = ticket1.getQuantity();
-    	assertEquals(2.0, verifquantityproduct);
-    	
-    }
-    
+   
     @Test
    
     public void testNoDiscount () {
@@ -229,22 +181,6 @@ public class SupermarketTest {
 
     }
     
-    @Test
-    public void getDescriptiontest(){
-        Product apples = new Product("apples", ProductUnit.Kilo);
-        Discount testDiscount = new Discount(apples, "Apples Discount", 1.99);
-
-        Assertions.assertThat(testDiscount.getDescription()).isEqualTo("Apples Discount");
-    }
- 
-    
-    @Test
-    public void getProducttest(){
-        Product apples = new Product("apples",ProductUnit.Kilo);
-        Discount testDiscount = new Discount(apples, "Apples Discount", 1.99);
-
-        Assertions.assertThat(testDiscount.getProduct()).isEqualTo(apples);
-    }
     
     
     @Test

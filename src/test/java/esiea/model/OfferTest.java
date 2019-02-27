241f55e050eb13eeff402e2738615fa31ff9a1d3
charlescoh <ccohen@et.esiea.fr>
2019-02-27 14:37:26 +0100

    ajout des premiers test pour la nouvelle feature (TDD)

--- src/test/java/esiea/model/OfferTest.java
+++ src/test/java/esiea/model/OfferTest.java
@@ -1,6 +1,9 @@
 package esiea.model;
 
 import org.junit.jupiter.api.Test;
+
+import java.awt.List;
+
 import org.assertj.core.api.Assertions;
 
 public class OfferTest {
@@ -16,6 +19,25 @@ public class OfferTest {
 
 		Assertions.assertThat(Myoffer.getProduct().getName()).isNotEqualTo("toothbrush");
 
+	}
+
+	@Test
+	public void TestMethodGetAllProducts() {
+		//TODO : methode getAllProducts() dans la classe Offer
+		
+		List<Product> product_offer = new ArrayList<Product>();
+		List<Product> product_in_cart = new ArrayList<Product>();
+		Product poire = new Product("poire", ProductUnit.Kilo);
+		Product pomme = new Product("pomme", ProductUnit.Kilo);
+		product_offer.add(poire);
+		product_offer.add(pomme);
+		product_in_cart.add(poire);
+		product_in_cart.add(pomme);
+		Offer myOffer = new Offer(SpecialOfferType.Bundle, product_offer, 10.0);
+		
+		Assertions.assertThat(product_offer.contains(poire));
+		
+		Assertions.assertThat(myOffer.getAllProducts().containsAll(product_offer));
 	}
 	
 }
